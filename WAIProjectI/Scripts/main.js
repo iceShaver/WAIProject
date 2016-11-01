1f52b0e3037e260e363ddaf3d71261deae13ba0a
Kamil Królikowski <kamil-krolikowski@outlook.com>
2016-11-01 22:51:34 +0100

    Initial Commit

---
+++ WAIProjectI/Scripts/main.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    var navY = $('nav').offset().top;
+    var stickyNav = function () {
+        var scrollY = $(window).scrollTop();
+
+        if (scrollY > navY) {
+            $('nav').addClass('nav-docked');
+            $('.container').addClass('container-nav-docked');
+        }
+        else {
+            $('nav').removeClass('nav-docked');
+            $('.container').removeClass('container-nav-docked');
+        }
+
+    };
+
+
+    stickyNav();
+
+    $(window).scroll(function () {
+        stickyNav();
+    });
+});
