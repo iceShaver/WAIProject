f2962f6057e45d067b13cc9184ccb2fb9e49c130
Kamil Królikowski <kamil-krolikowski@outlook.com>
2016-11-19 20:00:36 +0100

    new js functions: history

--- WAIProjectI/Scripts/main.js
+++ WAIProjectI/Scripts/main.js
@@ -1,4 +1,38 @@
-﻿$(document).ready(function () {
+﻿
+function addPageToHistory() {
+    if (sessionStorage.length <= 10)
+        sessionStorage.setItem("historyPage" + sessionStorage.length, document.title);
+    else {
+        for (var i = 0; i < sessionStorage.length - 1; i++) {
+            sessionStorage["historyPage" + i] = sessionStorage["historyPage" + (i + 1)];
+        }
+        console.log("historyPage" + (sessionStorage.length - 1));
+        sessionStorage.removeItem("historyPage" + (sessionStorage.length - 1));
+        sessionStorage.length--;
+        sessionStorage.setItem("historyPage" + sessionStorage.length, document.title);
+
+    }
+}
+document.addEventListener("DOMContentLoaded", function () {
+    addPageToHistory();
+    getHistory();
+
+
+
+});
+function getHistory() {
+    var pages = document.createDocumentFragment();
+    for (var i = sessionStorage.length - 1; i >= 0; i--) {
+        var p = document.createElement('p');
+        p.appendChild(document.createTextNode(sessionStorage.getItem("historyPage" + i)));
+        pages.appendChild(p);
+    }
+
+    var col1 = document.getElementById("col1-content");
+    col1.appendChild(pages);
+}
+
+$(document).ready(function () {
     var navY = $('nav').offset().top;
     var stickyNav = function () {
         var scrollY = $(window).scrollTop();
